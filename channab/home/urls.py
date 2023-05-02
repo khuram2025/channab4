724d6babbad91c1a5a4d00b6a215a79dbeb00dae
khuram2025 <khuram2025@gmail.com>
2023-05-02 19:41:25 +0300

    Finance Module is Working

--- channab/home/urls.py
+++ channab/home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    
 ]
--- channab/home/views.py
+++ channab/home/views.py
@@ -1,17 +1,63 @@
+# views.py
 from django.shortcuts import render
-from farm_finances.models import Income, Expense
+from farm_finances.models import Income, Expense, IncomeCategory, ExpenseCategory
 from django.db.models import Sum
-
+from datetime import timedelta, date
+from django.utils import timezone
 
 def home_view(request):
     total_income = Income.objects.all().aggregate(Sum('amount'))['amount__sum'] or 0
     total_expense = Expense.objects.all().aggregate(Sum('amount'))['amount__sum'] or 0
     status = total_income - total_expense
 
+    now = timezone.now()
+    default_time_range = now - timedelta(days=30)  # Default: last 30 days
+    time_ranges = {
+        "last_7_days": now - timedelta(days=7),
+        "last_30_days": now - timedelta(days=30),
+        "this_month": now.replace(day=1) - timedelta(days=1),
+        "this_year": now.replace(month=1, day=1) - timedelta(days=1),
+    }
+
+    # Get time filter from the request
+    time_filter = request.GET.get('time_filter', 'last_30_days')
+    selected_time_range = time_ranges.get(time_filter, default_time_range)
+
+    income_categories = IncomeCategory.objects.all()
+    expense_categories = ExpenseCategory.objects.all()
+
+
+    summary = []
+    for category in income_categories:
+        total_amount = Income.objects.filter(
+            category=category,
+            date__gte=selected_time_range
+        ).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
+
+        summary.append({
+            "category": category,
+            "total_amount": total_amount,
+        })
+
+    expense_summary = []
+    for category in expense_categories:
+        total_amount = Expense.objects.filter(
+            category=category,
+            date__gte=selected_time_range
+        ).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
+
+        expense_summary.append({
+            "category": category,
+            "total_amount": total_amount,
+        })
+
     context = {
         'total_income': total_income,
         'total_expense': total_expense,
-        'status': status
+        'status': status,
+        "summary": summary,
+        "time_filter": time_filter,
+        "expense_summary": expense_summary,
     }
 
     return render(request, 'home/index.html', context)
