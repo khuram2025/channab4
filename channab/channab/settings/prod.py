f8ff2ebe44b3174c9cfb9f993522a750507b1599
khuram2025 <khuram2025@gmail.com>
2023-05-25 08:57:08 +0300

    fine tuninng

--- channab/channab/settings/prod.py
+++ channab/channab/settings/prod.py
@@ -1,4 +1,6 @@
 from .base import *
+from pathlib import Path
+
 ALLOWED_HOSTS = ['*']
 DEBUG = True
 
@@ -13,7 +15,6 @@ DATABASES = {
     }
 }
 
-
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
@@ -22,14 +23,13 @@ AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
 
-AWS_LOCATION = 'static'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_URL = 'https://%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_ROOT = '/var/www/example.com/static/'
-MEDIA_URL = f"https://{'channabdairy.s3.amazonaws.com'}/media/"
+# Keep static files local
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Add this line at the end of the file
+STATICFILES_DIRS = [BASE_DIR / "static"]
