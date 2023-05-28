2f67c54e57938001d0237e90182ba405f5439cff
khuram2025 <khuram2025@gmail.com>
2023-05-28 10:13:39 +0300

    1

--- channab/channab/settings/prod.py
+++ channab/channab/settings/prod.py
@@ -1,5 +1,10 @@
 from .base import *
 from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 
 ALLOWED_HOSTS = ['*']
 DEBUG = True
