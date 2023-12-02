c0aeb0d29e50d26d11edeb31bfa4291819a6bde1
khuram <khuram2025@gmail.com>
2023-12-02 07:52:21 +0300

    admin finance file changed

--- channab/farm_finances/admin.py
+++ channab/farm_finances/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 
-from .models import Expense, Income
+from .models import Expense, ExpenseCategory, Income, IncomeCategory
 
-admin.site.register(Expense)
-admin.site.register(Income)
+@admin.register(IncomeCategory)
+class IncomeCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'farm']
+    # Add any other admin options here
+
+@admin.register(ExpenseCategory)
+class ExpenseCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'farm']
+    # Add any other admin options here
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'farm', 'date', 'description', 'amount', 'category', 'milk_payment']
+    # Add any other admin options here
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ['user', 'farm', 'date', 'description', 'amount', 'category', 'salary_transaction']
+    # Add any other admin options here
